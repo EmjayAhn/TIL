534b72353bf6e5b3ffae47798db3554370f8fb51
EmjayAhn <emjay.data@gmail.com>
2019-06-21 23:59:57 +0900

    Feat: 190621 Javascript

--- 08_Javascript/01_VanillaJS/03_first_app/greeting.js
+++ 08_Javascript/01_VanillaJS/03_first_app/greeting.js
@@ -1,7 +1,27 @@
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
-function init() {
+const greeting = document.querySelector(".js-greetings");
 
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
 }
 
 init();
